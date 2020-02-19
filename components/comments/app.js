5bf93f6c6a7d4696625afa8cfe695d80f647e6a2
MyNameIsBond <hajdinim@uni.coventry.ac.uk>
2020-02-19 18:37:48 +0000

    Api call changed for pipeline Vol2

--- components/comments/app.js
+++ components/comments/app.js
@@ -1,83 +1,92 @@
-import React, {useState} from "react";
+import React, { useState } from 'react'
 
-import Button from '@material-ui/core/Button';
-import Grid from '@material-ui/core/Grid';
-import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button'
+import Grid from '@material-ui/core/Grid'
+import TextField from '@material-ui/core/TextField'
 
 const App = props => {
-  
   const [name, setName] = useState(String)
   const [start_date, setDate] = useState(new Date('2014-08-18T21:11:54'))
   const [end_date, setEndDate] = useState(new Date('2014-08-18T21:11:54'))
 
   const handleDateChange = date => {
     console.log('!!')
-  };
+  }
 
-  const registerEvent = ()=> {
-
-    // const body = {
-    //   name: name,
-    //   start_date: start_date,
-    //   end_date: end_date
-    // }
-
-   const  body = {  
-      id: "122222",
-      buzz: "1222222",
+  const registerEvent = () => {
+    const body = {
+      id: '122222',
+      buzz: '1222222',
       title: "Ardavan's Empire",
-      description: "I love Microsoft",
-      abbreviation: "LA",
-      start_date: "14/02/2020",
-      end_date: "15/02/2020",
-      date_created: "23/01/2020",
-      date_modified: "22/01/2020"
-      }
-
-      fetch('https://livebus.azurewebsites.net/odata/Events', {
-      method: 'POST', // or 'PUT'
-      headers: {
-        'Content-Type': 'application/json',
+      description: 'I love Microsoft',
+      abbreviation: 'LA',
+      start_date: '14/02/2020',
+      end_date: '15/02/2020',
+      date_created: '23/01/2020',
+      date_modified: '22/01/2020'
+    }
+    const body2 = {
+      Event: {
+        title: "Tony's Speach",
+        description: 'Macs are better than windows',
+        abbreviation: 'macOS vs Windows',
+        start_date: '12/12/12',
+        end_date: '12/12/12',
+        date_created: '12/12/12',
+        date_modified: '12/12/12'
       },
-      body: JSON.stringify(body),
-      })
-      .then((response) => response.json(response))
-      .then((data) => {
-        console.log('Success:', data);
-      })
-      .catch((error) => {
-        console.error('Error:', error);
-      });
-
-      
+      Address: {
+        line_1: 'this is line 1',
+        line_2: 'this is line 2',
+        line_3: 'this is line 3',
+        line_4: 'this is line 4',
+        postcode: 'CV5 8DU',
+        address_note: 'Earlsdon St 86'
       }
-  
-return (
+    }
+    fetch(
+      'https://prod-31.uksouth.logic.azure.com:443/workflows/13ac2ac87d3e4ea3aabe8d6c6b5981cf/triggers/manual/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=_NuQ-zY3Uu-JQYASlcIGFfqpA-dNTSFIUQMo20HCM8M',
+      {
+        method: 'POST', // or 'PUT'
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(body2)
+      }
+    )
+      .then(response => response.json(response))
+      .then(data => {
+        console.log('Success:', data)
+      })
+      .catch(error => {
+        console.error('Error:', error)
+      })
+  }
+
+  return (
     <Grid container spacing={3}>
-        <Grid item xs={12}>
-        <TextField onChange={e => {
-          setName(e.target.value)
-          }} id="standard-basic" label="Event Title">
-      </TextField>
-        </Grid>
-        <Grid item xs={12}>
-        <TextField id="standard-basic" label="Event Description">
-      </TextField>
-        </Grid>
-        <Grid item xs={12}>
-        <TextField id="standard-basic" label="Event Abreviation">
-      </TextField>
-        </Grid>
-        <Grid item xs={12}>
+      <Grid item xs={12}>
+        <TextField
+          onChange={e => {
+            setName(e.target.value)
+          }}
+          id="standard-basic"
+          label="Event Title"
+        ></TextField>
+      </Grid>
+      <Grid item xs={12}>
+        <TextField id="standard-basic" label="Event Description"></TextField>
+      </Grid>
+      <Grid item xs={12}>
+        <TextField id="standard-basic" label="Event Abreviation"></TextField>
+      </Grid>
+      <Grid item xs={12}>
         <Button onClick={registerEvent} variant="contained" color="primary">
-       Register
-      </Button>
-        </Grid>
+          Register
+        </Button>
+      </Grid>
     </Grid>
   )
 }
 
-export default App;
-
-
-
+export default App
---
+++ components/navbar/app.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './styles.css'
+import {} from '@mate'
+const App = props => (
+  <div className={styles.special}>
+    <h1>Hello world</h1>
+  </div>
+)
+
+export default App
